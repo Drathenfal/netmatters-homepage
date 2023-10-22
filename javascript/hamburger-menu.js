ac0c55f3620c33bc5e61e6ea9c3d9e088cfa1751
Jake Bignell <jake.bignell@netmatters-scs.com>
2023-10-22 05:25:18 +0100

    IGNORE TTHIIS SHIR WONR DOR IR
    IGNORE TTHIIS SHIR WONR DOR IR

---
+++ javascript/hamburger-menu.js
@@ -0,0 +1,22 @@
+//hamburger menus
+
+const sideMenu1 = document.querySelector('.side-menu-1');
+const sideMenu2 = document.querySelector('.side-menu-2');
+const hamburger = document.querySelector('.hamburger');
+const darkOverlayMenu = document.querySelector('.dark-overlay-menu');
+const content = document.querySelector('.content');
+
+function toggleMenu() {
+    sideMenu1.classList.toggle('active');
+    sideMenu2.classList.toggle('active');
+    darkOverlayMenu.classList.toggle('active');
+    content.classList.toggle('active');
+    if (sideMenu1.classList.contains('active')) {
+        content.style.marginRight = "280px";
+    } else {
+        content.style.marginRight = "0";
+    }
+}
+
+hamburger.addEventListener('click', toggleMenu);
+darkOverlayMenu.addEventListener('click', toggleMenu);
