5bb9d09d0cb01d67aca5a1d1a7db613b3de8462d
Jake Bignell <jake.bignell@netmatters-scs.com>
2023-10-24 16:03:40 +0100

    Slick banner semi working
    Slick banner semi working

---
+++ javascript/slick-banner.js
@@ -0,0 +1,14 @@
+// slick banner
+
+$(document).ready(function(){
+    $('.banner').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: false
+    });
+});
